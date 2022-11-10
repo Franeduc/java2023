83203a201336204cbd6ae96a790b9d140e3bf92b
usuario1daw <franeduc1@gmail.com>
2022-11-10 21:51:03 +0100

    primer ejercicio

---
+++ PruebaGitHub/src/Prueba/Ejercicio2.java
@@ -0,0 +1,30 @@
+package Prueba;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		//Declaracion de variables
+		//TIPO- NOMBRE - VALOR POR DEFECTO
+		
+		short num1 = 8;
+		int mes = 12;
+		long distancia = 300000;
+		
+		char letra = 'a';
+		float largo = 1e10f;
+		double precio = 35000.95;
+		
+		boolean ganasDeIrme = true;
+		
+		System.out.println(num1);
+		System.out.println(mes);
+		System.out.println(distancia);
+		System.out.println(letra);
+		System.out.println(largo);
+		System.out.println(precio);
+		System.out.println(ganasDeIrme);
+		
+	}
+
+}
