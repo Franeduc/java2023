b3ad44f3a64db39a618f6a2fa8ed6cac7d5bd1e1
usuario1daw <franeduc1@gmail.com>
2022-12-14 18:01:18 +0100

    wawa

---
+++ PruebaGitHub/src/Practica3/Ejercicio10.java
@@ -0,0 +1,12 @@
+package Practica3;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		//Tabla de multiplicar numero entero 1-10 introducido por telclado
+		
+		
+
+	}
+
+}
---
+++ PruebaGitHub/src/Practica4/Ejercicio14.java
@@ -0,0 +1,31 @@
+package Practica4;
+
+import java.util.Scanner;
+
+public class Ejercicio14 {
+
+	public static void main(String[] args) {
+		String palabra = "";
+		String palabraSinCaracterFinal = "";
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce una palabra");
+		
+		try {
+				palabra = sc.nextLine();
+		} catch (Exception e) {
+				System.out.println(e.getLocalizedMessage());
+		}
+		
+		//Girar la cadena
+		StringBuffer sb = new StringBuffer(palabra);
+		sb.reverse();
+		
+		palabraSinCaracterFinal = palabra.substring(0, palabra.length() - 1);
+		
+		System.out.println(palabraSinCaracterFinal + sb);
+
+	}
+
+}
